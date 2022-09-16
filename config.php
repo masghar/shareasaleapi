4f0b7ce57b76834f7cbfcd48613cd365cd10fa62
Muhammad Asghar <m.muhammad.asghar@gmail.com>
2022-09-17 01:08:45 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'shareddb-y.hosting.stackcp.net');
-define('DB_USERNAME', 'sasapi-3135391c30');
-define('DB_PASSWORD', 'zvhync2day');
-define('DB_NAME', 'sasapi-3135391c30');
+define('DB_SERVER', 'YOUR DB HOST');
+define('DB_USERNAME', 'USERNAME');
+define('DB_PASSWORD', 'PASSWORD');
+define('DB_NAME', 'DBNAME');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
